2b6b89d1da022a42697b976fbfe580da8b1000a9
Roman <romalupan89@gmail.com>
2022-11-07 21:52:45 +0200

    Features page icons and buttons

--- src/Features.jsx
+++ src/Features.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import Bill from './assets/bill.png';
+import Monitor from './assets/monitor.png';
+import Organize from './assets/organize.png';
 
 const Features = () => {
   return (
@@ -8,16 +11,25 @@ const Features = () => {
           Features
         </h3>
         <div className="features">
-          <ul className="flex w-full flex-col items-center gap-6 text-lg">
-            <li>Lorem ipsum dolor sit amet.</li>
-            <li>Lorem ipsum dolor sit amet.</li>
-            <li>Lorem ipsum dolor sit amet.</li>
+          <ul className="flex text-center flex-col items-center gap-6 text-lg ml-8 mr-8">
+            <li className="flex">
+              <img src={Bill} alt="icon of a bill" className="h-9 w-9 mr-2" />
+              Track bills and see how much you spend
+            </li>
+            <li className="flex">
+              <img src={Monitor} alt="" className="h-9 w-9 mr-2" />
+              Get statistic data and analysis
+            </li>
+            <li className="flex">
+              <img src={Organize} alt="" className="h-9 w-9 mr-2" />
+              Organize your budget for your bills
+            </li>
           </ul>
           <div className="btns flex justify-center gap-14 mt-24 font-semibold ">
-            <button className="bg-white h-9 w-24 rounded-md cursor-pointer">
+            <button className="btn bg-white h-9 w-24 rounded-md cursor-pointer">
               Log In
             </button>
-            <button className="bg-white h-9 w-24 rounded-md cursor-pointer">
+            <button className="btn bg-white h-9 w-24 rounded-md cursor-pointer">
               Sign Up
             </button>
           </div>
