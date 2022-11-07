0decdd2804aaa8ae01a72762d382c54cd89b2664
Roman <romalupan89@gmail.com>
2022-11-07 19:53:04 +0200

    Features Page

--- src/Features.jsx
+++ src/Features.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
 
 const Features = () => {
   return (
-    <div>Features</div>
-  )
-}
+    <>
+      <div>
+        <h3 className="flex w-full justify-center mt-16 text-xl font-bold mb-16 ">
+          Features
+        </h3>
+        <div className="features">
+          <ul className="flex w-full flex-col items-center gap-6 text-lg">
+            <li>Lorem ipsum dolor sit amet.</li>
+            <li>Lorem ipsum dolor sit amet.</li>
+            <li>Lorem ipsum dolor sit amet.</li>
+          </ul>
+          <div className="btns flex justify-center gap-14 mt-24 font-semibold ">
+            <button className="bg-white h-9 w-24 rounded-md cursor-pointer">
+              Log In
+            </button>
+            <button className="bg-white h-9 w-24 rounded-md cursor-pointer">
+              Sign Up
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Features
+export default Features;
